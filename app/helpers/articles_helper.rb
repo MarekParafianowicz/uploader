a02a4310ee07771118cae64d8e98b1907e7ce816
marek parafianowicz <marek.parafianowicz@netguru.co>
2017-03-12 20:06:55 +0100

    Use rails helpers

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,10 @@
 module ArticlesHelper
+
+  def content(article)
+    article.paper.blank? ? "No content provided" : article.paper
+  end
+
+  def picture(article)
+    article.file.blank? ? "No picture provided" : image_tag(article.file.thumb.url.to_s)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,4 @@
 class Article < ActiveRecord::Base
   validates :name, presence: true
   mount_uploader :file, FileUploader
-
-  def content
-    paper.blank? ? "No content provided" : paper
-  end
-
-  def picture
-    file.blank? ? "No picture provided" : ActionController::Base.helpers.image_tag(self.file.thumb.url.to_s)
-  end
 end
